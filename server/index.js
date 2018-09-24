20b16dd61a0b158bef78c68d301466cbc902b472
SergeyRazzhivin <razjiwin2010@yandex.ru>
2018-09-24 21:01:19 +0300

    Lessons Video Sessions

--- server/index.js
+++ server/index.js
@@ -7,11 +7,39 @@ import webpackMiddleware from "webpack-dev-middleware";
 import webpackHotMiddleware from "webpack-hot-middleware";
 import webpackConfig from "../webpack.config.dev";
 import auth from "./routes/auth";
-// import session from "express-session";
-// import KnexSessionStore from "connect-session-knex";
+import mongoose from "mongoose";
+import session from "express-session";
+// Connect mongo DB
+mongoose.connect(
+  "mongodb://admin:torial1988@ds046037.mlab.com:46037/lessonsdb",
+  { useNewUrlParser: true }
+);
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+// Session
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+  })
+);
+
+app.get("/", function(req, res, next) {
+  if (req.session.views) {
+    req.session.views++;
+    res.setHeader("Content-Type", "text/html");
+    res.write(`<p>views: ${req.session.views}</p>`);
+    res.write(`<p>expires in: ${req.session.cookie.maxAge / 1000} s</p>`);
+    res.end();
+  } else {
+    req.session.views = 1;
+    res.end("welcome to the session demo. refresh!");
+  }
+});
 
 app.use(history());
 app.use(bodyParser.json());
